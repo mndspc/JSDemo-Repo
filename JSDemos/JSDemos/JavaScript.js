af1363c1abac12de3fd9d8c146e608344eef6ea9
Shoaib.Ghachi <shoaib.ghachi@hotmail.com>
2021-08-31 16:35:44 +0530

    Push New Project

---
+++ JSDemos/JSDemos/JavaScript.js
@@ -0,0 +1,12 @@
+﻿function Sum(cntid1, cntid2) {
+    var num1 = document.getElementById(cntid1).value;
+    var num2 = document.getElementById(cntid2).value;
+    var result =parseInt( num1) +parseInt( num2);
+    return result;
+}
+function Sum1(cntid1, cntid2) {
+    var num1 = document.getElementById(cntid1).value;
+    var num2 = document.getElementById(cntid2).value;
+    var result = parseInt(num1) + parseInt(num2);
+    alert(result);
+}
---
+++ JSDemos/JSDemos/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("JSDemos")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JSDemos")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7e9fc11a-21bf-4333-b62c-1f6034b58a94")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
